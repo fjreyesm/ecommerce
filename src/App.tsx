fbf2f9b5a5458f8a13fcd4532971ac192d844412
fjreyesm <franklin.reyes.2003@gmail.com>
2022-07-27 14:55:58 +0200

    project arquitecture

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Store } from "./pages/Store";
+
+function App() {
+  return (
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Container>
+  );
+}
+
+export default App;
