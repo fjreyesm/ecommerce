6aabac5b1736a1f6e2ea4cb14c0a41fffe38e5db
fjreyesm <franklin.reyes.2003@gmail.com>
2022-07-28 09:25:28 +0200

    add assets,data and map items in Store

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,22 @@
+import { Col, Row } from "react-bootstrap";
+import stoteItems from "../data/items.json";
+
 export const Store = () => {
   return (
-    <div>
-      <h1>Store</h1>
-    </div>
+    <>
+      <div>
+        <h1>Store</h1>
+        <Row md={2} xs={1} lg={3} className="g-3">
+          {stoteItems.map((item) => (
+            <Col key={item.id}>
+              <img src={item.imgUrl} alt={item.name} />
+              <h3>{item.name}</h3>
+              <p>{item.description}</p>
+              <p>{item.price}</p>
+            </Col>
+          ))}
+        </Row>
+      </div>
+    </>
   );
 };
