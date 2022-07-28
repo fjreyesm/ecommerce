55067f065b4e8d66110aa29e134253f7956ce4a0
fjreyesm <franklin.reyes.2003@gmail.com>
2022-07-28 12:36:27 +0200

    estyling store

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,18 +1,16 @@
 import { Col, Row } from "react-bootstrap";
-import stoteItems from "../data/items.json";
+import { StoreItem } from "../components/StoreItem";
+import storeItems from "../data/items.json";
 
 export const Store = () => {
   return (
     <>
       <div>
-        <h1>Store</h1>
+        <h1>VR Store</h1>
         <Row md={2} xs={1} lg={3} className="g-3">
-          {stoteItems.map((item) => (
+          {storeItems.map((item) => (
             <Col key={item.id}>
-              <img src={item.imgUrl} alt={item.name} />
-              <h3>{item.name}</h3>
-              <p>{item.description}</p>
-              <p>{item.price}</p>
+              <StoreItem {...item} />
             </Col>
           ))}
         </Row>
