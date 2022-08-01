8edd4305008033af4f9518cdd739cfcc67be1cf5
fjreyesm <franklin.reyes.2003@gmail.com>
2022-08-01 09:49:35 +0200

    shopping cart total

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,11 +1,41 @@
-import { Offcanvas, OffcanvasHeader, OffcanvasTitle } from "react-bootstrap";
+import {
+  Offcanvas,
+  OffcanvasBody,
+  OffcanvasHeader,
+  OffcanvasTitle,
+  Stack,
+} from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import { formatCurrency } from "../utilities/formatCoin";
+import { CartItem } from "./CartItem";
 
-export function ShoppingCart() {
+type ShoppingCartProps = {
+  isOpen: boolean;
+};
+
+export function ShoppingCart({ isOpen }: ShoppingCartProps) {
+  const { closeCart, cartItems } = useShoppingCart();
   return (
-    <Offcanvas show={true} placement="end">
-      <OffcanvasHeader closeButton>
-        <OffcanvasTitle> Shopping Cart</OffcanvasTitle>
-      </OffcanvasHeader>
+    <Offcanvas show={isOpen} onHide="closeCart" placement="end">
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title> Shopping Cart</Offcanvas.Title>
+        <Offcanvas.Body>
+          <Stack gap={3}>
+            {cartItems.map((item) => (
+              <CartItem key={item.id} {...item} />
+            ))}
+            <div className="ms-auto fw-bold fs-5">
+              Total: $
+              {formatCurrency(
+                cartItems.reduce((total, cartItem) => {
+                  const item = storeItems.find((i) => i.id === cartItem.id);
+                  return total + (item?.price || 0) * cartItem.quantity;
+                }, 0)
+              )}
+            </div>
+          </Stack>
+        </Offcanvas.Body>
+      </Offcanvas.Header>
     </Offcanvas>
   );
 }
--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -89,7 +89,7 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
       }}
     >
       {children}
-      <ShoppingCart />
+      <ShoppingCart isOpen={isOpen} />
     </ShoppingCartContext.Provider>
   );
 }
